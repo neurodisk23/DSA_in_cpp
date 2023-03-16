3601681f44ee50a450e062856f43115315d7cf55
neurodisk23 <ai21mtech02003@iith.ac.in>
2023-03-16 19:40:19 +0530

    added continue statement

---
+++ Primitive_data_types/control_statements_break_and_continue.cpp
@@ -0,0 +1,16 @@
+// Funtion to check for an odd day 
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int date;
+    cout<<"Please input the date"<<endl;
+    cin>> date;
+
+    for(date)
+    
+    return 0;
+}
